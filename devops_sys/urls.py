0c1589e486b8aefb73482819184d17d585f4f0f4
xiaotuanyu120 <zhaopeiwu@outlook.com>
2016-03-10 19:58:19 -0500

    20160312

--- devops_sys/urls.py
+++ devops_sys/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+import asset.views
 
 urlpatterns = [
+    url(r'^$', asset.views.index),
     url(r'^admin/', admin.site.urls),
 ]
